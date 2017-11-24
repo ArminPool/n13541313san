a36460b15622b33fde7229c06bd11d491c8473e5
ArminPool <armin.oldboy@gmail.com>
2017-11-24 05:53:26 +0330

    dev and prod settings changed

--- news/setting/dev.py
+++ news/setting/dev.py
@@ -1,3 +1,15 @@
 from ..settings import *
 
 DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'arminoldboy$navasan',
+        'USER': 'arminoldboy',
+        'PASSWORD': '123555888mysql',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'HOST':'arminoldboy.mysql.pythonanywhere-services.com'
+    }
+}
--- news/setting/prod.py
+++ news/setting/prod.py
@@ -4,11 +4,12 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'navasan',
-        'USER': 'armin.oldboy',
+        'NAME': 'arminoldboy$navasan',
+        'USER': 'arminoldboy',
         'PASSWORD': '123555888mysql',
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
+        'HOST':'arminoldboy.mysql.pythonanywhere-services.com'
     }
 }
