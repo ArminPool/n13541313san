486373c6f1185b4b75ed20a76c0378cdeeddeff1
ArminPool <armin.oldboy@gmail.com>
2017-11-28 11:07:11 +0330

    wsgi_produ changed

--- news/wsgi_produ.py
+++ news/wsgi_produ.py
@@ -1,7 +1,19 @@
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = u'/home/navasang/navasangold'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'news.settings'
+
+
+# serve django via WSGI in Django < 1.7
+# import django.core.handlers.wsgi
+# application = django.core.handlers.wsgi.WSGIHandler()
+# serve django via WSGI in Django >= 1.7
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
