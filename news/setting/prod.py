89c690f4cd8c99936a2ebef5fa31566528184fee
ArminPool <armin.oldboy@gmail.com>
2017-12-09 12:07:16 +0330

    wsgi_prod changed

--- news/setting/prod.py
+++ news/setting/prod.py
@@ -1,3 +1,18 @@
 from ..settings import *
 
 DEBUG = False
+ALLOWED_HOSTS = [u'navasangold.com', 'www.navasangold.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'navasang_navasangold',
+        'USER': 'navasang_armin',
+        'PASSWORD': '123555888armin',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        'HOST': 'localhost'
+
+    }
+}
