705ad59dc75f323ce7f03eef67e9b1bdc29c0aea
ArminPool <armin.oldboy@gmail.com>
2017-12-11 08:07:55 +0330

    Debug True for testing

--- news/setting/prod.py
+++ news/setting/prod.py
@@ -10,7 +10,6 @@ DATABASES = {
         'PASSWORD': '123555888armin',
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
         'HOST': 'localhost'
 
