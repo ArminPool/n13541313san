a656f2922c147b1f0daf1629a056d91229545643
ArminPool <armin.oldboy@gmail.com>
2017-12-12 12:41:47 +0330

    setting db changed

--- news/settings.py
+++ news/settings.py
@@ -85,7 +85,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'navasangold',
+            'NAME': 'navasan',
             'USER': 'root',
             'PASSWORD': '123555888mysql',
             'OPTIONS': {
@@ -104,7 +104,6 @@ else:
             'PASSWORD': '123555888armin',
             'OPTIONS': {
                 'charset': 'utf8mb4',
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             },
             'HOST': 'localhost'
 
