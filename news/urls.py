77db6b58990f60979a04a1486fb9c1fcb489ec99
ArminPool <armin.oldboy@gmail.com>
2017-12-13 13:50:13 +0330

    description tag added egain

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 
-from news.sitemap import PostsSitemap
+from news.sitemap import PostsSitemap, StaticSitemap
 from posts.views import search, economic_calender, calender
 from specificpages.views import specific_pages
 from users.views import contact
@@ -13,7 +13,7 @@ from . import settings
 
 sitemaps = {
     'post': PostsSitemap(),
-
+    'static': StaticSitemap(),
 }
 urlpatterns = [
 
