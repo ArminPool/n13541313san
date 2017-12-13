7279e786453a6dc7e69565f1a5f56b6ce9e04ef7
ArminPool <armin.oldboy@gmail.com>
2017-12-13 14:12:58 +0330

    urls edited

--- news/urls.py
+++ news/urls.py
@@ -47,5 +47,5 @@ urlpatterns = [
 
                   url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
                       name='django.contrib.sitemaps.views.sitemap'), ] + staticfiles_urlpatterns() + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+      settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
