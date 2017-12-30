dd40e4e96f3567bf459b9769e766ca9da08680c9
ArminPool <armin.oldboy@gmail.com>
2017-12-30 13:12:00 +0330

    margin added to post-text img and author username in detail changed to first name + last name

--- news/urls.py
+++ news/urls.py
@@ -13,9 +13,6 @@ from specificpages.views import specific_pages, tariffs
 from users.views import contact, all_vip_registered
 from . import settings
 
-
-
-
 sitemaps = {
     'post': PostsSitemap(),
     'static': StaticSitemap(),
@@ -27,23 +24,24 @@ urlpatterns = [
     url(r'', include('posts.urls')),
     url(r'^user/', include('users.urls')),
     url(r'^search/', search, name='search'),
-    url(r'^software/metatrader/$', TemplateView.as_view(template_name='specificpages/metatrader.html'),name='metatrader'),
-    url(r'^rules/$', TemplateView.as_view(template_name='specificpages/rules.html'),name='rules'),
-    url(r'^about-us/$', TemplateView.as_view(template_name='specificpages/about-us.html'),name='about-us'),
-    url(r'^tariffs/$', tariffs,name='tariffs'),
+    url(r'^software/metatrader/$', TemplateView.as_view(template_name='specificpages/metatrader.html'),
+        name='metatrader'),
+    url(r'^rules/$', TemplateView.as_view(template_name='specificpages/rules.html'), name='rules'),
+    url(r'^about-us/$', TemplateView.as_view(template_name='specificpages/about-us.html'), name='about-us'),
+    url(r'^tariffs/$', tariffs, name='tariffs'),
 
-    url(r'^mql5/$', TemplateView.as_view(template_name='specificpages/mql.html'),name='mql'),
-    url(r'^pamm/$', TemplateView.as_view(template_name='specificpages/pamm.html'),name='pamm'),
-    url(r'^chart/GOLD/$', TemplateView.as_view(template_name='specificpages/GOLD.html'),name='GOLD'),
-    url(r'^chart/EURUSD/$', TemplateView.as_view(template_name='specificpages/EURUSD.html'),name='EURUSD'),
-    url(r'^chart/USDJPY/$', TemplateView.as_view(template_name='specificpages/USDJPY.html'),name='USDJPY'),
-    url(r'^chart/GBPUSD/$', TemplateView.as_view(template_name='specificpages/GBPUSD.html'),name='GBPUSD'),
+    url(r'^mql5/$', TemplateView.as_view(template_name='specificpages/mql.html'), name='mql'),
+    url(r'^pamm/$', TemplateView.as_view(template_name='specificpages/pamm.html'), name='pamm'),
+    url(r'^chart/GOLD/$', TemplateView.as_view(template_name='specificpages/GOLD.html'), name='GOLD'),
+    url(r'^chart/EURUSD/$', TemplateView.as_view(template_name='specificpages/EURUSD.html'), name='EURUSD'),
+    url(r'^chart/USDJPY/$', TemplateView.as_view(template_name='specificpages/USDJPY.html'), name='USDJPY'),
+    url(r'^chart/GBPUSD/$', TemplateView.as_view(template_name='specificpages/GBPUSD.html'), name='GBPUSD'),
     url(r'^chart/USDCAD/$', TemplateView.as_view(template_name='specificpages/USDCAD.html'), name='USDCAD'),
 
-    url(r'^chart/USDCHF/$', TemplateView.as_view(template_name='specificpages/USDCHF.html'),name='USDCHF'),
-    url(r'^chart/AUDUSD/$', TemplateView.as_view(template_name='specificpages/AUDUSD.html'),name='AUDUSD'),
-    url(r'^chart/NZDUSD/$', TemplateView.as_view(template_name='specificpages/NZDUSD.html'),name='NZDUSD'),
-    url(r'^chart/BTCUSD/$', TemplateView.as_view(template_name='specificpages/BTCUSD.html'),name='BTCUSD'),
+    url(r'^chart/USDCHF/$', TemplateView.as_view(template_name='specificpages/USDCHF.html'), name='USDCHF'),
+    url(r'^chart/AUDUSD/$', TemplateView.as_view(template_name='specificpages/AUDUSD.html'), name='AUDUSD'),
+    url(r'^chart/NZDUSD/$', TemplateView.as_view(template_name='specificpages/NZDUSD.html'), name='NZDUSD'),
+    url(r'^chart/BTCUSD/$', TemplateView.as_view(template_name='specificpages/BTCUSD.html'), name='BTCUSD'),
 
     url(r'^economicCalendar/$', economic_calender, name='economic_calendar'),
     url(r'^contact-us/$', contact, name='Contact'),
