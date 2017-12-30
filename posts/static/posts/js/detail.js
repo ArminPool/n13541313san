396bf545fc181a80aed6ce1cda7e953c54e69fa0
ArminPool <armin.oldboy@gmail.com>
2017-12-30 11:11:01 +0330

    detail.js going to fix

--- posts/static/posts/js/detail.js
+++ posts/static/posts/js/detail.js
@@ -1,31 +1,51 @@
-$(function () {
+$(document).ready(function() {
+closeNav();
+var clicked;
+$(".comments span + button").click(function(event){
+    event.preventDefault();
+    $(this).nextAll('form').first().fadeToggle();
 
-  $(".js-upload-photos").click(function () {
-    $("#fileupload").click();
-  });
 
-  $("#fileupload").fileupload({
-    dataType: 'json',
-    sequentialUploads: true,  /* 1. SEND THE FILES ONE BY ONE */
-    start: function (e) {  /* 2. WHEN THE UPLOADING PROCESS STARTS, SHOW THE MODAL */
-      $("#modal-progress").modal("show");
-    },
-    stop: function (e) {  /* 3. WHEN THE UPLOADING PROCESS FINALIZE, HIDE THE MODAL */
-      $("#modal-progress").modal("hide");
-    },
-    progressall: function (e, data) {  /* 4. UPDATE THE PROGRESS BAR */
-      var progress = parseInt(data.loaded / data.total * 100, 10);
-      var strProgress = progress + "%";
-      $(".progress-bar").css({"width": strProgress});
-      $(".progress-bar").text(strProgress);
-    },
-    done: function (e, data) {
-      if (data.result.is_valid) {
-        $("#gallery tbody").prepend(
-          "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
-        )
-      }
-    }
+});
+
+$("#post-text p img").addClass('img-responsive');
+
+$(".show-replies").click(function(event){
+    event.preventDefault();
+     $(this).nextAll('.see-replies').first().fadeToggle();
+
+});
+ $("textarea").before("<label>پیام</label>");
+
+
+$('blockquote').filter(function(){return $(this).text().trim().length==0}).remove();
+
+function closeNav(){
+
+sideNav = 2;
+var ScreenWidth = $(window).width();
+if(ScreenWidth < 586){
+$("footer").css("display","block");
+$("#main").css("display","block");
+
+
+$("#SideNav").css("width","0px");
+$("#left-side-toggle-open").css("display","inline");
+
+$("#left-side-toggle-close").css("display","none");
+
+}
+else{
+
+$("footer").css("display","block");
+$("#main").css("display","block");
+$("#main").css("margin-right","0px");
+$("#SideNav").css("width","0px");
+$("#left-side-toggle-open").css("display","inline");
+
+$("#left-side-toggle-close").css("display","none");
+
+}
 
   });
 
