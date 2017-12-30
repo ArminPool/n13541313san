d10f7a57d3f401b6b70b424939f61137a1b263d6
ArminPool <armin.oldboy@gmail.com>
2017-12-30 11:38:20 +0330

    detail js  edited

--- posts/static/posts/js/detail.js
+++ posts/static/posts/js/detail.js
@@ -8,9 +8,9 @@ $(".comments span + button").click(function(event){
 
 });
 
-$("#post-text p img").addClass('img-responsive');
+$("#post-text p img").addClass('img-responsive').addClass('img-inside-text');
 $("#post-text p img").removeAttr("style");
-$("#post-text p img").parent('p').first().css('text-align','center');
+$(".img-inside-text").parent('p').first().css('text-align','center');
 $(".show-replies").click(function(event){
     event.preventDefault();
      $(this).nextAll('.see-replies').first().fadeToggle();
