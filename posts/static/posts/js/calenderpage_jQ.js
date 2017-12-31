1ce982a53e263e3afc6f1143790f66ccf441ab40
ArminPool <armin.oldboy@gmail.com>
2017-12-31 11:22:37 +0330

    calender

--- posts/static/posts/js/calenderpage_jQ.js
+++ posts/static/posts/js/calenderpage_jQ.js
@@ -1,64 +1,6 @@
 $(document).ready(function() {
 
 
-
-
-
-    if($('#main').height() < 340 ){
-
-
-    $('footer').css('position','absolute');
-     $('footer').css('bottom','0px');
-    }
-    else{
-    }
-
-$(window).on('resize', function(){
-
-   if($('#main').height() < 340 ){
-
-
-    $('footer').css('position','absolute');
-     $('footer').css('bottom','0px');
-    }
-
-
-else{
-
- $('footer').css('position','relative');
-}
-
-});
-
-
-
-
-
-
-
-
-$(window).on('resize', function(){
-
-   if($('#main').height() < 340 ){
-
-
-    $('footer').css('position','absolute');
-     $('footer').css('bottom','0px');
-    }
-
-
-else{
-
- $('footer').css('position','relative');
-}
-
-});
-
-
-
-
-
-$("footer").css("bottom","0px");
 var d = new Date();
 var month = d.getMonth()+1;
 var day = d.getDate();
