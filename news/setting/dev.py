4c034cfc828976e9f4783f4678b8b4b09cd6670f
ArminPool <armin.oldboy@gmail.com>
2018-01-21 15:50:03 +0330

    dev setting testing

--- news/setting/dev.py
+++ news/setting/dev.py
@@ -1,3 +1,22 @@
-from .prod import *
+from ..settings import *
 
 DEBUG = True
+
+ALLOWED_HOSTS = [u'navasangold.com', 'www.navasangold.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'navasang_navasangold',
+        'USER': 'navasang_armin',
+        'PASSWORD': '123555888armin',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'HOST': 'localhost'
+
+    }
+}
+
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+MEDIA_URL = '/static/media/'
+
